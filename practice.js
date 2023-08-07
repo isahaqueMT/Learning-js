98bdc609208c10175766782a993730ccab845102
Md Isahaque <ishaquemahmud81@gmail.com>
2023-08-07 15:43:15 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,63 @@
+// array
+
+// var fdName = ["ishak", "jaman", "kamal"];
+// console.log (fdName);
+
+// var fdName = ["ishak", "jaman", "kamal"];
+// console.log (fdName[0]);
+
+// var fdName = ["ishak", "jaman", "kamal"];
+// fdName[1] = "mahmud";
+// console.log (fdName);
+
+// var fdName = ["ishak", "jaman", "kamal"];
+// fdName.push("tonoy");
+// console.log (fdName);
+
+// var fdName = ["ishak", "jaman", "kamal"];
+// fdName.pop();
+// console.log (fdName);
+
+// var fdName = ["ishak", "jaman", "kamal"];
+// fdName.shifat();
+// console.log (fdName);
+
+// var fdName = ["ishak", "jaman", "kamal"];
+// fdName.unshift(100);
+// console.log (fdName);
+
+// var fdName = ["ishak", "jaman", "kamal"];
+// console.log (fdName.slice(0,1));
+
+// var fdName = ["ishak", "jaman", "kamal"];
+// fdName.splice(1,3, "mahmud", "tonoy");
+// console.log (fdName);
+
+// var fdName = ["ishak", "jaman", "kamal"];
+// fdName.splice(1,3, "mahmud", "tonoy");
+// console.log (fdName);
+
+// loop ===========================
+
+// for(var sum = 0; sum <= 5; sum++){
+//     console.log(sum);
+// }
+
+// var num = [0,1,2,3,4,5,6,7,8, "ishak", "mahmud", "tonoy" ];
+// for(var i = 0; i <num.length; i++){
+//     console.log(num[i]);
+// }
+
+// var i = 0;
+// while(i < 9){
+//     console.log(i);
+//     i++
+// }
+
+
+// function=======================
+
+function say(message){
+    console.log(message);
+}
+say()
